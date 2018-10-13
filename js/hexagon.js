93489b0fd32c3a78775510566b0577dcfb7156ea
Thomas Tursics <thomas@tursics.de>
2018-10-13 16:19:07 +0200

    show a hexagon grid

--- js/hexagon.js
+++ js/hexagon.js
@@ -1,5 +1,5 @@
 /*jslint browser: true*/
-/*global $,window,waterTank*/
+/*global $,window*/
 
 //-----------------------------------------------------------------------
 
@@ -7,25 +7,79 @@ var hexagon = {
 
 	parent: null,
 	items: [],
+	columns: 8,
 
 	//-------------------------------------------------------------------
 
 	init: function (parent) {
 		'use strict';
 
-		this.parent = parent;
+		function addPumpStation(index, tank) {
+			var selector = 'hex' + index;
 
-		var i, str = '', selector;
-
-		for (i = 0; i < waterTank.length(); ++i) {
-			selector = 'hex' + i;
-			this.items.push({
+			hexagon.items.push({
 				selector: '#' + selector,
-				tank: i
+				tank: tank
 			});
 
-			str += '<div id="' + selector + '" class="hex pumpStation fill0 turbine on3"></div>';
+			return '<div id="' + selector + '" class="hex pumpStation fill0 turbine on3"></div>';
 		}
+
+		function addGras(index) {
+			var selector = 'hex' + index;
+
+			hexagon.items.push({
+				selector: '#' + selector,
+				tank: null
+			});
+
+			return '<div id="' + selector + '" class="hex"></div>';
+		}
+
+		this.parent = parent;
+
+		var i = 0, str = '';
+
+		str += addPumpStation(i++, 0);
+		str += addGras(i++);
+		str += addPumpStation(i++, 1);
+		str += addGras(i++);
+		str += addGras(i++);
+		str += addGras(i++);
+		str += addGras(i++);
+		str += addGras(i++);
+		str += addPumpStation(i++, 2);
+		str += addPumpStation(i++, 3);
+		str += addGras(i++);
+		str += addGras(i++);
+		str += addGras(i++);
+		str += addGras(i++);
+		str += addGras(i++);
+		str += addGras(i++);
+		str += addPumpStation(i++, 4);
+		str += addGras(i++);
+		str += addPumpStation(i++, 5);
+		str += addGras(i++);
+		str += addGras(i++);
+		str += addGras(i++);
+		str += addGras(i++);
+		str += addGras(i++);
+		str += addGras(i++);
+		str += addPumpStation(i++, 6);
+		str += addPumpStation(i++, 7);
+		str += addGras(i++);
+		str += addGras(i++);
+		str += addGras(i++);
+		str += addGras(i++);
+		str += addGras(i++);
+		str += addPumpStation(i++, 8);
+		str += addGras(i++);
+		str += addPumpStation(i++, 9);
+		str += addGras(i++);
+		str += addGras(i++);
+		str += addGras(i++);
+		str += addGras(i++);
+		str += addGras(i++);
 
 		this.parent.html(this.parent.html() + str);
 	},
@@ -83,10 +137,10 @@ var hexagon = {
 		var i, line;
 
 		for (i = 0; i < this.items.length; ++i) {
-			line = Math.trunc(i / 2);
+			line = Math.trunc(i / this.columns);
 			$(this.items[i].selector).css({
-				'left': 2 + 2.5 * ((i % 2) + (line % 2) * 0.5) + 'em',
-				'top': 2 + 2.15 * (line) + 'em',
+				'left': 2 + 2.5 * ((i % this.columns) + (line % 2) * 0.5) + 'em',
+				'top': 0.5 + 2.15 * (line) + 'em',
 				'z-index': 50 + line
 			});
 		}

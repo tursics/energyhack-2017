1e2557adf82953207f9924980cf1884e72b45766
Thomas Tursics <thomas@tursics.de>
2020-09-18 23:18:32 +0200

    add basis data for pump stations

---
+++ src/pumpstation.js
@@ -0,0 +1,107 @@
+//-----------------------------------------------------------------------
+
+// storageSpace: Stauvolumen in m³
+// q16: Q16 in l/s
+// qMax: Qmax in l/s
+// data: in l /s
+
+export pumpStations = [
+	{
+		title: 'Pumpwerk 1',
+		storageSpace: 10300,
+		q16: 701,
+		qMax: 1403,
+	},
+	{
+		title: 'Pumpwerk 2',
+		storageSpace: 8812,
+		q16: 210,
+		qMax: 420,
+	},
+	{
+		title: 'Pumpwerk 3',
+		storageSpace: 7850,
+		q16: 434,
+		qMax: 868,
+	},
+	{
+		title: 'Pumpwerk 4',
+		storageSpace: 2532,
+		q16: 106,
+		qMax: 212,
+	},
+	{
+		title: 'Pumpwerk 5',
+		storageSpace: 14600,
+		q16: 123,
+		qMax: 247,
+	},
+	{
+		title: 'Pumpwerk 6',
+		storageSpace: 7390,
+		q16: 250,
+		qMax: 500,
+	},
+	{
+		title: 'Pumpwerk 7',
+		storageSpace: 5880,
+		q16: 214,
+		qMax: 427,
+	},
+	{
+		title: 'Pumpwerk 8',
+		storageSpace: 4069,
+		q16: 250,
+		qMax: 500,
+	},
+	{
+		title: 'Pumpwerk 9',
+		storageSpace: 3488,
+		q16: 97,
+		qMax: 194,
+	},
+	{
+		title: 'Pumpwerk 10',
+		storageSpace: 1914,
+		q16: 299,
+		qMax: 597,
+	},
+	{
+		title: 'Pumpwerk 11',
+		storageSpace: 1770,
+		q16: 92,
+		qMax: 184,
+	},
+	{
+		title: 'Pumpwerk 12',
+		storageSpace: 4700,
+		q16: 50,
+		qMax: 101,
+	},
+	{
+		title: 'Pumpwerk 13',
+		storageSpace: 8583,
+		q16: 318,
+		qMax: 635,
+	},
+	{
+		title: 'Pumpwerk 14',
+		storageSpace: 4094,
+		q16: 236,
+		qMax: 472,
+	},
+	{
+		title: 'Pumpwerk 15',
+		storageSpace: 2902,
+		q16: 394,
+		qMax: 788,
+	},
+	{
+		title: 'Pumpwerk 16',
+		storageSpace: 1742,
+		q16: 200,
+		qMax: 399,
+	},
+];
+
+//-----------------------------------------------------------------------

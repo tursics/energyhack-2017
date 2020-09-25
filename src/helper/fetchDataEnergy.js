d42eb820038de2efbbab6e76d5907786e89a4f77
Thomas Tursics <thomas@tursics.de>
2020-09-26 00:31:01 +0200

    fetch data

--- src/helper/fetchDataEnergy.js
+++ src/helper/fetchDataEnergy.js
@@ -2,193 +2,98 @@
 // https://www.stromnetz.berlin/globalassets/dokumente/opendata/erlaeuterungen-livedaten-zugriff-stromnetz-berlin-smeter_engine.pdf
 //----------------------------
 
-function getEnergyData() {
-	console.log('getEnergyData');
-
+function getDataFromStromnetze(options, next, reject) {
 	var https = require('https');
-	var postData = `
-<smeterengine>
- <scale>DAY</scale>
- <city>BERLIN</city>
- <district name='Spandau'>
- <time_period begin="2013-06-05 00:00:00" end="2013-06-12 23:59:59" time_zone='CET'/>
-</district>
-</smeterengine> 
-	`;
-	var options = {
+	var requestOptions = {
 		host: 'smeterengine.stromnetz.berlin',
+		path: options.path /*+ 'mobile'*/,
+		method: 'POST',
+		headers: {
+			'Content-Type': 'text/xml;charset=UTF-8',
+			'Content-Length': options.xmlData.length,
+		}
+	};
+
+	var request = https.request(requestOptions, function (res) {
+		var data = '';
+		res.on('data', function (chunk) {
+			data += chunk;
+		});
+		res.on('end', function () {
+			next(data);
+		});
+	});
+
+	request.on('error', function (e) {
+		reject(e.message);
+	});
+	request.write(options.xmlData);
+	request.end();
+}
+
+//----------------------------
+
+function getEnergyData(date, next, reject) {
+	// <district name='Spandau'>
+
+	var dateBegin = date.getFullYear() + '-' + ('0' + date.getMonth()).slice(-2) + '-' + ('0' + date.getDate()).slice(-2) + ' 00:00:00';
+	var dateEnd = date.getFullYear() + '-' + ('0' + date.getMonth()).slice(-2) + '-' + ('0' + date.getDate()).slice(-2) + ' 23:59:59';
+	var options = {
 		path: '/SmeterEngine/networkcontrol',
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded',
-			'Content-Length': postData.length
-		}
+		xmlData:`
+			<smeterengine>
+				<scale>DAY</scale>
+				<city>BERLIN</city>
+				<district>
+					<time_period begin="${dateBegin}" end="${dateEnd}" time_zone='CET'/>
+				</district>
+			</smeterengine>`,
 	};
-	var request = https.request(options, function (res) {
-		var data = '';
-		res.on('data', function (chunk) {
-			data += chunk;
-		});
-		res.on('end', function () {
-			console.log(data);
-		});
-	});
-
-	request.on('error', function (e) {
-		console.log(e.message);
-	});
-	request.write(postData);
-	request.end();
+	getDataFromStromnetze(options, next, reject);
 }
 
-function getWeatherData() {
-	console.log('getWeatherData');
+//----------------------------
 
-	var https = require('https');
-/*	var options = {
-		host: 'www.vattenfall.de',
-		path: '/SmeterEngine/energyProjection'
-	};*/
-	var postData = `
-<smeterengine start="2013-06-12T09:00:00" end="2013-06-12T11:00:00">
- <cities>
- <city>BERLIN</city>
- <latitude>52.30</latitude>
- <longitude>13.25</longitude>
- </cities>
-</smeterengine> 
-	`;
+function getWeatherData(date, next, reject) {
+	var dateBegin = date.getFullYear() + '-' + ('0' + date.getMonth()).slice(-2) + '-' + ('0' + date.getDate()).slice(-2) + 'T00:00:00';
+	var dateEnd = date.getFullYear() + '-' + ('0' + date.getMonth()).slice(-2) + '-' + ('0' + date.getDate()).slice(-2) + 'T23:59:59';
 	var options = {
-		host: 'smeterengine.stromnetz.berlin',
 		path: '/SmeterEngine/energyProjection',
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded',
-			'Content-Length': postData.length
-		}
+		xmlData: `
+			<smeterengine start="${dateBegin}" end="${dateEnd}">
+				<cities>
+					<city>BERLIN</city>
+					<latitude>52.30</latitude>
+					<longitude>13.25</longitude>
+				</cities>
+			</smeterengine>`,
 	};
-	var request = https.request(options, function (res) {
-		var data = '';
-		res.on('data', function (chunk) {
-			data += chunk;
+	getDataFromStromnetze(options, next, reject);
+}
+
+//----------------------------
+
+function processOneYear(year, next) {
+	var date = new Date(year, 0, 1);
+
+	function processNext() {
+		getEnergyData(date, () => {
+			date = date.setDate(date.getDate() + 1);
+			if (date.getFullYear === year) {
+				processNext();
+			} else {
+				next();
+			}
 		});
-		res.on('end', function () {
-			console.log(data);
-		});
-	});
-
-	request.on('error', function (e) {
-		console.log(e.message);
-	});
-	request.write(postData);
-	request.end();
-}
-
-//----------------------------
-
-function getData() {
-	var http = require('http');
-
-	var options = {
-		host: 'www.vattenfall.de',
-//		path: '/SmeterEngine/networkcontrol'
-		path: '/SmeterEngine/energyProjection'
 	}
-var request = http.request(options, function (res) {
-    var data = '';
-    res.on('data', function (chunk) {
-        data += chunk;
-    });
-    res.on('end', function () {
-        console.log(data);
-
-    });
-});
-request.on('error', function (e) {
-    console.log(e.message);
-});
-request.end();
+	processNext();
 }
 
 //----------------------------
 
-function test()
-{
-	try {
-/*		txt = '';
-		txt += '<form action="https://www.vattenfall.de/SmeterEngine/energyProjection" method="post">';
-		txt += 'Username: <input type="text" name="user">';
-		txt += '<input type="submit" value="Submit">';
-		txt += '</form>';
-		$( "#ulHome").html( txt);
-		$( "#ulHome").listview('refresh');*/
-
-/*		$.ajax({
-			type: 'POST',
-			url: 'https://www.vattenfall.de/SmeterEngine/energyProjection',
-//			contentType: "application/x-www-form-urlencoded; charset=utf-8",
-			datatype: 'xml',
-			data:
-'<smeterengine start="2013-06-12T09:00:00" end="2013-06-12T11:00:00">'+
-'<cities>'+
-'<city>BERLIN</city>'+
-'<latitude>52.30</latitude>'+
-'<longitude>13.25</longitude>'+
-'</cities>'+
-'</smeterengine>',
-			cache: false,
-			success: function( result) {
-				alert(result);
-				try {
-					$( "#textParse").html( result);
-				} catch(e) {
-					$( "#textParse").html( 'Unknown metadata format found');
-				}
-			},
-			error: function( jqxhr, textStatus, error) {
-				alert(textStatus);
-				$( "#textParse").html( 'Please download ' + this.url + ' and past the content here.');
-//							alert( this.url);
-			},
-		});*/
-
-$.ajax({
-    url: 'http://www.vattenfall.de/SmeterEngine/networkcontrol',
-//    url: 'http://www.vattenfall.de/SmeterEngine/energyProjection',
-    data: 
-'<smeterengine>'+
-'<scale>DAY</scale>'+
-'<city>BERLIN</city>'+
-'<district>'+
-'<time_period begin="2013-06-12 15:40:00" end="2013-06-12 17:00:00" time_zone="CET"/>'+
-'</district>'+
-'</smeterengine>',
-//'<smeterengine start="2013-06-12T09:00:00" end="2013-06-12T11:00:00">'+
-//'<cities>'+
-//'<city>BERLIN</city>'+
-//'<latitude>52.30</latitude>'+
-//'<longitude>13.25</longitude>'+
-//'</cities>'+
-//'</smeterengine>',
-    type: 'POST',
-    contentType: "text/xml",
-    dataType: "text/xml",
-    success : function( result) {
-				alert(result);
-			},
-    error : function (xhr, ajaxOptions, thrownError){  
-        console.log(xhr.status);          
-        console.log(thrownError);
-				alert(ajaxOptions);
-    } 
-}); 
-
-	} catch( e) {
-		alert(e);
-	}
-}
+var date = new Date(2013, 6 - 1, 12);
+getEnergyData(date, (data) => console.log(data.length), (e) => console.error(e));
+//	.reject(message => console.log(message));
+//getWeatherData();
 
 //----------------------------
-
-getEnergyData();
-getWeatherData();
